36e4cdea7653a680ff010f4b7c5b389a63082980
Sazeduzzaman <szamansaju@gmail.com>
2026-07-29 02:07:30 +0600

    error solved

--- src/data/FooterData.ts
+++ src/data/FooterData.ts
@@ -1,35 +1,32 @@
 interface DataType {
-   id: number;
-   title: string;
-   nav_links: {
-      link: string;
-      title: string
-   }[];
-}[];
+  id: number;
+  title: string;
+  nav_links: {
+    link: string;
+    title: string;
+  }[];
+}
 
 const footer_data: DataType[] = [
-   {
-      id: 1,
-      title: "Service",
-      nav_links: [
-         { link: "service", title: "Web design" },
-         { link: "service", title: "Digital marketing" },
-         { link: "service", title: "It management" },
-         { link: "service", title: "Cloud services" },
-         { link: "service", title: "Machine learning" },
-      ],
-   },
-   {
-      id: 2,
-      title: "Useful link",
-      nav_links: [
-         { link: "about", title: "About Us" },
-         { link: "team", title: "Team" },
-         { link: "project", title: "Portfolio" },
-         { link: "service", title: "Service" },
-         { link: "contact", title: "Contact Us" },
-      ]
-   },
-]
+  {
+    id: 1,
+    title: "Services",
+    nav_links: [
+      { link: "/service", title: "Nursing Care" },
+      { link: "/service", title: "PCA & HHA Care" },
+      { link: "/service", title: "OPWDD Services" },
+      { link: "/service", title: "Pediatric Care" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Useful Links",
+    nav_links: [
+      { link: "/about", title: "About Us" },
+      { link: "/service", title: "Our Services" },
+      { link: "/contact", title: "Contact Us" },
+    ],
+  },
+];
 
 export default footer_data;
